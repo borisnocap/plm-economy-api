b279e101803338021c250bb8ddb56d917b7f8369
borisnocap <no-email@email.com>
2023-04-21 00:00:22 +0400

    Интерфейс возвращен к виду похожему на интерфейс, без заранее написанных методов Изменены типы некоторых переменных и методов

--- src/main/java/ru/plm/economy/EconomyAPI.java
+++ src/main/java/ru/plm/economy/EconomyAPI.java
@@ -3,56 +3,14 @@ package ru.plm.economy;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.UUID;
 
 public interface EconomyAPI {
 
-    HashMap<UUID, EconomyTransaction> economyTransactions = new HashMap<>();
-    HashMap<String, Long> playersBucksBalances = new HashMap<>();
-    HashMap<String, Long> playersPlumcoinsBalances = new HashMap<>();
+    long createTransaction(String... transactionParticipants);
 
-    // Создание новой экономической транзакции.
-    // Взаимодействие с балансами игроков возможно только при наличии экономической транзакции, внутрь которой
-    // записываются все выполняемые операции.
-    default UUID createEconomyTransaction() {
-        EconomyTransaction economyTransaction = new EconomyTransaction();
-        economyTransactions.put(economyTransaction.getUUID(), economyTransaction);
-        return economyTransaction.getUUID();
-    }
+    boolean cancelTransaction(long transactionId);
 
-    // Отмена экономической транзакции.
-    // В случае вызова этого метода экономическая транзакция закрывается и удаляется без выполнения записанных
-    // в нее операций (изменений балансов игроков не будут применены к кэшу).
-    default void cancelEconomyTransaction(UUID uuid) throws EconomyException {
-        EconomyTransaction economyTransaction = economyTransactions.remove(uuid);
-        if (economyTransaction ==  null) throw new EconomyException("Экономическая транзакция не найдена.");
-        if (!economyTransaction.isClosed()) economyTransaction.close();
-    }
-
-    // Закрытие существующей экономической транзакции.
-    // После закрытия транзакции все записанные в нее операции с балансом применяются к кэшу плагина.
-    default void closeEconomyTransaction(UUID uuid) throws EconomyException {
-        EconomyTransaction economyTransaction = economyTransactions.remove(uuid);
-        if (economyTransaction ==  null) throw new EconomyException("Отсутствует экономическая транзакция.");
-        if (economyTransaction.isClosed()) throw new EconomyException("Экономическая транзакция уже закрыта.");
-        economyTransaction.close();
-        economyTransaction.getOperations().forEach(operation -> {
-            String player = operation.getPlayerName();
-            switch (operation.getCurrency()) {
-                case BUCKS -> {
-                    long oldBalance = playersBucksBalances.get(player);
-                    long newBalance = oldBalance + operation.getAmount();
-                    playersBucksBalances.put(player, newBalance);
-                }
-                case PLUMCOINS -> {
-                    long oldBalance = playersPlumcoinsBalances.get(player);
-                    long newBalance = oldBalance + operation.getAmount();
-                    playersPlumcoinsBalances.put(player, newBalance);
-                }
-            }
-        });
-    }
+    void closeTransaction(long transactionId) throws EconomyException;
 
     String formatBucks(long amount);
 
@@ -62,22 +20,11 @@ public interface EconomyAPI {
 
     boolean hasBucks(String playerName, long amount);
 
-    // Сохранение баланса баксов игрока в кэше плагина (используется только после старта плагина).
-    // Метод выбросит ошибку, если вызвать его, когда баланс игрока уже находится в кэше.
-    default void setBucksBalance(UUID txUUID, String playerName, long balance) throws EconomyException {
-        if (!economyTransactions.containsKey(txUUID)) throw new EconomyException("Отсутствует экономическая транзакция.");
-        if (playersBucksBalances.containsKey(playerName)) throw new EconomyException("Запрещено устанавливать баланс игрока, если баланс уже был инициализирован.");
-        EconomyTransactionOperation operation = new EconomyTransactionOperation(playerName, EconomyTransactionOperation.Currency.BUCKS, EconomyTransactionOperation.UpdateType.DEPOSIT, balance);
-        economyTransactions.get(txUUID).getOperations().add(operation);
-    }
+    void setBucksBalance(long transactionId, String playerName, long balance) throws EconomyException;
 
-    // Начисление баксов игроку.
-    // Начисление должно быть записано в БД в виде применения апдейта (executeUpdate();) к SQL-транзакции (опция setAutoCommit(false);).
-    void depositBucks(Connection conn, PreparedStatement ps, UUID transactionUUID, String playerName, long amount) throws SQLException;
+    void depositBucks(Connection conn, PreparedStatement ps, long transactionId, String playerName, long amount) throws EconomyException, SQLException;
 
-    // Списание баксов у игрока.
-    // Списание должно быть записано в БД в виде применения апдейта (executeUpdate();) к SQL-транзакции (опция setAutoCommit(false);).
-    void withdrawBucks(Connection conn, PreparedStatement ps, UUID transactionUUID, String playerName, long amount) throws SQLException;
+    void withdrawBucks(Connection conn, PreparedStatement ps, long transactionId, String playerName, long amount) throws EconomyException, SQLException;
 
     String formatPlumcoins(long amount);
 
@@ -87,21 +34,9 @@ public interface EconomyAPI {
 
     boolean hasPlumcoins(String playerName, long amount);
 
-    // Сохранение баланса плюмкоинов игрока в кэше плагина (используется только после старта плагина).
-    // Метод выбросит ошибку, если вызвать его, когда баланс игрока уже находится в кэше.
-    default void setPlumcoinsBalance(UUID txUUID, String playerName, long balance) throws EconomyException {
-        if (!economyTransactions.containsKey(txUUID)) throw new EconomyException("Отсутствует экономическая транзакция.");
-        if (playersPlumcoinsBalances.containsKey(playerName)) throw new EconomyException("Запрещено устанавливать баланс игрока, если баланс уже был инициализирован.");
-        EconomyTransactionOperation operation = new EconomyTransactionOperation(playerName, EconomyTransactionOperation.Currency.PLUMCOINS, EconomyTransactionOperation.UpdateType.DEPOSIT, balance);
-        economyTransactions.get(txUUID).getOperations().add(operation);
-    }
+    void setPlumcoinsBalance(long transactionId, String playerName, long balance) throws EconomyException;
 
-    // Начисление плюмкоинов игроку.
-    // Начисление должно быть записано в БД в виде применения апдейта (executeUpdate();) к SQL-транзакции (опция setAutoCommit(false);).
-    void depositPlumcoins(Connection conn, PreparedStatement ps, UUID transactionUUID, String playerName, long amount) throws SQLException;
+    void depositPlumcoins(Connection conn, PreparedStatement ps, long transactionId, String playerName, long amount) throws EconomyException, SQLException;
 
-
-    // Списание плюмкоинов у игрока.
-    // Списание должно быть записано в БД в виде применения апдейта (executeUpdate();) к SQL-транзакции (опция setAutoCommit(false);).
-    void withdrawPlumcoins(Connection conn, PreparedStatement ps, UUID transactionUUID, String playerName, long amount) throws SQLException;
+    void withdrawPlumcoins(Connection conn, PreparedStatement ps, long transactionId, String playerName, long amount) throws EconomyException, SQLException;
 }
--- src/main/java/ru/plm/economy/EconomyException.java
+++ src/main/java/ru/plm/economy/EconomyException.java
@@ -2,10 +2,6 @@ package ru.plm.economy;
 
 public class EconomyException extends Exception {
 
-    public EconomyException() {
-        super();
-    }
-
     public EconomyException(String message) {
         super(message);
     }
