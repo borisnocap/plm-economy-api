80311e929e3a51c5422cabc00af7f2ffaf6a39da
borisnocap <no-email@email.com>
2023-04-26 05:49:37 +0400

    Создан метод writeTransactionToDatabase

--- src/main/java/ru/plm/economy/EconomyAPI.java
+++ src/main/java/ru/plm/economy/EconomyAPI.java
@@ -6,12 +6,6 @@ import java.util.UUID;
 
 public interface EconomyAPI {
 
-    UUID createTransaction(Currency currency, String... participants);
-
-    boolean cancelTransaction(UUID transactionUUID);
-
-    void closeTransaction(Connection connection, UUID transactionUUID) throws EconomyException, SQLException;
-
     String formatCurrency(Currency currency, long amount);
 
     long getBalance(Currency currency, String playerName);
@@ -21,4 +15,12 @@ public interface EconomyAPI {
     void deposit(Currency currency, UUID transactionUUID, String playerName, long amount) throws EconomyException;
 
     void withdraw(Currency currency, UUID transactionUUID, String playerName, long amount) throws EconomyException;
+
+    UUID createTransaction(Currency currency, String... participants);
+
+    boolean cancelTransaction(UUID transactionUUID);
+
+    void writeTransactionToDatabase(Connection connection, UUID transactionUUID) throws EconomyException, SQLException;
+
+    void closeTransaction(UUID transactionUUID) throws EconomyException;
 }
