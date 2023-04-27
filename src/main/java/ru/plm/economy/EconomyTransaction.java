c7c05ba1c50c6e8df3644db6cfbf14ae9e7b1e4b
borisnocap <no-email@email.com>
2023-04-28 02:03:10 +0400

    В EconomyTransaction возвращены экспешены

--- src/main/java/ru/plm/economy/EconomyTransaction.java
+++ src/main/java/ru/plm/economy/EconomyTransaction.java
@@ -2,7 +2,7 @@ package ru.plm.economy;
 
 public interface EconomyTransaction {
 
-    void addWithdrawOperation(String playerName, long amount);
+    void addWithdrawOperation(String playerName, long amount) throws EconomyException;
 
-    void addDepositOperation(String playerName, long amount);
+    void addDepositOperation(String playerName, long amount) throws EconomyException;
 }
